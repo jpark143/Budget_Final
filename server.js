fcac53cd447a6b2b6455a3908c6e0bdca7de1d8c
Jae Park <jae@April.local>
2020-12-14 17:26:18 -0500

    api

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const path = require('path');
+
+app.use(bodyParser.json());
+
+const PORT = 3000;
+
+app.post('/api//public/login', (req, res) => {
+    const {username, password} = req.body;
+    console.log('This is me', username, password);
+    res.json({data: 'working'});
+    for (let user of users) {
+        if (username == user.username && password == user.password) {
+            let token = jwt.sign({ id: user.id, username: user.username }, secretKey, { expiresIn: '10d'});
+            res.json({
+                success: true,
+                err: null,
+                token
+            });
+            break;
+        }
+        else{
+            res.status(401).json({
+                success: false,
+                token: null,
+                err: 'Username or password is incorrect'
+            });
+        }
+    }
+});
+
+app.use('/login.html', express.static('public'));
+app.use('/', express.static('public'));
+app.use('/signup.html', express.static('public'));
+app.use('/dashboard.html', express.static('public'));
+app.use('/signup.html', express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/Home.html'));
+});
+
+app.get('/login.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/login.html'));
+});
+
+app.get('/signup.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/signup.html'));
+});
+
+app.get('/dashboard.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/dashboard.html'));
+});
+
+app.get('/signup_comp.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/signup_comp.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Serving on port ${PORT}`);
+});
