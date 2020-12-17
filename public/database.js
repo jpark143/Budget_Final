ed969930694b278524ce26c0b4b1283e1c9fffda
Jae Park <jae@April.local>
2020-12-17 13:42:26 -0500

    updated database

--- public/database.js
+++ public/database.js
@@ -1,10 +1,19 @@
-const mongoose = require('mongoose');
 
-const URI = "mongodb+srv://jae:K!llbe999@cluster0.rxovf.mongodb.net/budget_app?retryWrites=true&w=majority"
+var firebaseConfig = {
+    apiKey: "AIzaSyCz5a0RX_i3LGissCJrt3tDQtdLqGdo4Zg",
+    authDomain: "budget-d5244.firebaseapp.com",
+    projectId: "budget-d5244",
+    storageBucket: "budget-d5244.appspot.com",
+    messagingSenderId: "329653372520",
+    appId: "1:329653372520:web:50e176f336f3329efd9ddd",
+    measurementId: "G-VFBS3SNMRS"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
-const connectDB = async() => {
-    await mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true });
-    console.log('database connected');
-}
-
-module.exports = connectDB;
+  function addData(){
+      firebase.database().ref("Budget_Info").set({
+          totalBudgetmonth: document.getElementById("monthArea").value,
+          totalAmountBudg: document.getElementById("addBudget").value * 1
+      })
+  }
